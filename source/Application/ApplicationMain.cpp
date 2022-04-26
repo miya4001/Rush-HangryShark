00377c4986e3e83a5af10bd6219a7533890f582b
宮澤耀生 <121pg0023@tokyo.amg.ac.jp>
2022-04-26 16:11:45 +0900

    Application:アプリケーションメインクラス

--- source/Application/ApplicationMain.cpp
+++ source/Application/ApplicationMain.cpp
@@ -1,7 +1,29 @@
 /*****************************************************************//**
  * @file   ApplicationMain.cpp
- * @brief  
+ * @brief  アプリケーションメインクラス
  * 
  * @author 宮澤耀生
  * @date   April 2022
  *********************************************************************/
+#include "ApplicationMain.h"
+
+namespace Game {
+  namespace Application {
+    // アプリケーションの実態
+    ApplicationMain application;
+
+    ApplicationMain::ApplicationMain() : ApplicationBase() {
+      if (IsSetApplication()) {
+        // 実態生成
+        _application = std::make_shared<ApplicationMain>();
+      }
+    }
+
+    bool ApplicationMain::Init() {
+      if (!ApplicationBase::Init()) {
+        return false;  // 初期化失敗
+      }
+      return true;  // 初期化成功
+    }
+  } // namespace Application
+} // namespace Game
--- source/Application/ApplicationMain.h
+++ source/Application/ApplicationMain.h
@@ -1,8 +1,40 @@
 /*****************************************************************//**
  * @file   ApplicationMain.h
- * @brief  
+ * @brief  アプリケーションメインクラス
  * 
  * @author 宮澤耀生
  * @date   April 2022
  *********************************************************************/
 #pragma once
+#include <appframe.h>
+
+/**
+ * @brief  ゲーム
+ */
+namespace Game {
+  /**
+   * @brief  アプリケーション
+   */
+  namespace Application {
+    /**
+     * @class  ApplicationMain
+     * @brief  アプリケーションメインクラス
+     */
+    class ApplicationMain :public AppFrame::Application::ApplicationBase {
+    public:
+      /**
+       * @brief  コンストラクタ
+       */
+      ApplicationMain();
+      /**
+       * @brief  アプリケーションの初期化
+       * @return true:初期化成功
+       *         false:初期化失敗
+       */
+      bool Init() override;
+
+    private:
+
+    };
+  } // namespace Application
+} // namespace Game
