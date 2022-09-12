72ef29cbf61034de580eb48bc027db112013448b
宮澤耀生 <ysi.xx02@gmail.com>
2022-09-12 17:47:26 +0900

    Collision:球の衝突判定クラス

---
+++ source/Collision/CollisionSphere.cpp
@@ -0,0 +1,43 @@
+/*****************************************************************//**
+ * @file   CollisionSphere.cpp
+ * @brief  ‹…‚ÌÕ“Ë”»’èƒNƒ‰ƒX
+ * 
+ * @author ‹{àV—s¶
+ * @date   September 2022
+ *********************************************************************/
+#include "CollisionSphere.h"
+
+namespace Game {
+  namespace Collision {
+    CollisionSphere::CollisionSphere(Object::ObjectBase& owner, const AppMath::Vector4 center, const float radius) : _owner(owner) {
+      _position = center;
+      _radius = radius;
+    }
+
+    CollisionSphere::~CollisionSphere() {
+
+    }
+
+    void CollisionSphere::Process(AppMath::Vector4 move) {
+      // ’†SÀ•W‚ÉˆÚ“®—Ê‚ğ‰ÁZ
+      _position.Add(move);
+    }
+
+#ifdef _DEBUG
+    void CollisionSphere::Draw() const {
+      // ‹…‚Ì•`‰æ(Õ“Ë‚ ‚è‚Ìê‡‹…‚Ì“h‚è‚Â‚Ô‚µ)
+      DrawSphere3D(AppMath::UtilityDX::ToVECTOR(_position), _radius, 10, GetColor(0, 0, 0), 0, _collision);
+    }
+#endif
+
+    bool CollisionSphere::IntersectSphere(const CollisionSphere& sphere) {
+      // ‹…‚Æ‹…‚Ì‹——£‚Ì“ñæ
+      float distSquared = AppMath::Vector4::LengthSquared(_position - sphere._position);
+      // ‹…‚Æ‹…‚Ì”¼Œa‚Ì‡Œv
+      float sumRadius = _radius + sphere._radius;
+      // ‹——£‚Ì“ñæ‚ª”¼Œa‚Ì‡Œv‚Ì“ñæˆÈ‰º‚Ìê‡Õ“Ë
+      _collision = distSquared <= sumRadius * sumRadius;
+      return _collision;
+    }
+  } // namespace Collision
+} // namespace Game
---
+++ source/Collision/CollisionSphere.h
@@ -0,0 +1,78 @@
+/*****************************************************************//**
+ * @file   CollisionSphere.h
+ * @brief  球の衝突判定クラス
+ * 
+ * @author 宮澤耀生
+ * @date   September 2022
+ *********************************************************************/
+#pragma once
+#include <appframe.h>
+
+/**
+ * @brief  ゲーム
+ */
+namespace Game{
+  /**
+   * @brief  オブジェクト
+   */
+  namespace Object {
+    class ObjectBase;
+  } // namespace Object
+  /**
+   * @brief  衝突判定
+   */
+  namespace Collision {
+    namespace AppMath = AppFrame::Math;
+    /**
+     * @class CollisionSphere
+     * @brief 球の衝突判定クラス
+     */
+    class CollisionSphere {
+    public:
+      /**
+       * @brief  コンストラクタ
+       * @param  owner 所有者の参照
+       * @param  center 中心座標
+       * @param  radius 球の半径
+       */
+      CollisionSphere(Object::ObjectBase& owner, const AppMath::Vector4 center, const float radius);
+      /**
+       * @brief  デストラクタ
+       */
+      ~CollisionSphere();
+      /**
+       * @brief  更新
+       * @param  move 移動量
+       */
+      void Process(AppMath::Vector4 move);
+#ifdef _DEBUG
+      /**
+       * @brief  描画
+       */
+      void Draw() const;
+#endif
+      /**
+       * @brief  球と球の衝突判定
+       * @param  sphere 対象の球
+       * @return true:衝突あり
+       *         false:衝突なし
+       */
+      bool IntersectSphere(const CollisionSphere& sphere);
+      /**
+       * @brief  ローカル座標の設定
+       * @param  position 座標
+       */
+      inline void SetPosition(const AppMath::Vector4 position) {
+        _position = position;
+      }
+
+    private:
+      float _radius{ 0.0f };     //!< 球の半径
+      bool _collision{ false };  //!< 衝突判定
+      //!< 所有者の参照
+      Object::ObjectBase& _owner;
+      //!< ローカル座標
+      AppMath::Vector4 _position;
+    };
+  } // namespace Collision
+} // namespace Game
